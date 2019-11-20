e0b1db4cd087d0e0d4efb77b05322aeee326ee40
Robert M Ricci <rmricci121@gmail.com>
2019-11-20 18:48:32 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,7 @@ def nyc_pigeon_organizer(data)
   data.each do |first_level, all_other|
     all_other.each do |category, array|
       array.each do |name|
-    
+        organized_pigeons.
     end
   end
   end
